e0803218adc6f5b217dd3bfb2ef604585dae6c3b
Pierre Mdawar <pierre@mdawar.dev>
2024-10-17 13:55:53 +0300

    feat: add more test cases for string parsing

--- parse_test.go
+++ parse_test.go
@@ -12,59 +12,70 @@ import (
 )
 
 func BenchmarkParsing(b *testing.B) {
-	const value = "123.456"
+	cases := []string{
+		"0",
+		"1",
+		"0.31032345678911",
+		"1.23",
+		"123456.78910111213",
+		"12345678910.123456789101112131",
+	}
 
-	b.Run("shopspring", func(b *testing.B) {
-		for range b.N {
-			_, err := shopspring.NewFromString(value)
-			if err != nil {
-				b.Fatal(err)
-			}
-		}
-	})
+	for _, value := range cases {
+		b.Run(value, func(b *testing.B) {
+			b.Run("shopspring", func(b *testing.B) {
+				for range b.N {
+					_, err := shopspring.NewFromString(value)
+					if err != nil {
+						b.Fatal(err)
+					}
+				}
+			})
 
-	b.Run("alpacadecimal", func(b *testing.B) {
-		for range b.N {
-			_, err := alpacadecimal.NewFromString(value)
-			if err != nil {
-				b.Fatal(err)
-			}
-		}
-	})
+			b.Run("alpacadecimal", func(b *testing.B) {
+				for range b.N {
+					_, err := alpacadecimal.NewFromString(value)
+					if err != nil {
+						b.Fatal(err)
+					}
+				}
+			})
 
-	b.Run("apd", func(b *testing.B) {
-		for range b.N {
-			_, _, err := apd.NewFromString(value)
-			if err != nil {
-				b.Fatal(err)
-			}
-		}
-	})
+			b.Run("apd", func(b *testing.B) {
+				for range b.N {
+					_, _, err := apd.NewFromString(value)
+					if err != nil {
+						b.Fatal(err)
+					}
+				}
+			})
 
-	b.Run("ericlagergren", func(b *testing.B) {
-		for range b.N {
-			_, ok := new(ericlagergren.Big).SetString(value)
-			if !ok {
-				b.Fatal("parse failed")
-			}
-		}
-	})
+			b.Run("ericlagergren", func(b *testing.B) {
+				for range b.N {
+					_, ok := new(ericlagergren.Big).SetString(value)
+					if !ok {
+						b.Fatal("parse failed")
+					}
+				}
+			})
 
-	b.Run("govalues", func(b *testing.B) {
-		for range b.N {
-			_, err := govalues.Parse(value)
-			if err != nil {
-				b.Fatal(err)
-			}
-		}
-	})
+			b.Run("govalues", func(b *testing.B) {
+				for range b.N {
+					_, err := govalues.Parse(value)
+					if err != nil {
+						b.Fatal(err)
+					}
+				}
+			})
 
-	b.Run("udecimal", func(b *testing.B) {
-		for range b.N {
-			_, err := udecimal.Parse(value)
-			if err != nil {
-				b.Fatal(err)
-			}
-		}
-	})
+			b.Run("udecimal", func(b *testing.B) {
+				for range b.N {
+					_, err := udecimal.Parse(value)
+					if err != nil {
+						b.Fatal(err)
+					}
+				}
+			})
+		})
+	}
 }
